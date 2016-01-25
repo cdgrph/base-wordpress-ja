3b3ed93c778098eba8fa88b16e1a8cf9962efbf0
wataru <wataru@codegraph.jp>
2016-01-25 16:58:56 +0900

    load jquery and toolkit in footer

--- wp-content/themes/_s/functions.php
+++ wp-content/themes/_s/functions.php
@@ -120,6 +120,13 @@ function _s_scripts() {
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	// deregister default jQuery included with Wordpress
+	// wp_deregister_script( 'jquery' );
+	// $jquery_cdn = '//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js';
+	// wp_enqueue_script( 'jquery', $jquery_cdn, array(), '2.2.0', true );
+
+	// wp_enqueue_script( '_s-script', get_template_directory_uri() . '/js/script.min.js', array(), '', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
