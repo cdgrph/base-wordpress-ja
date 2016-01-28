da75d88e2841057ad741e70f4130d11da180dc32
wataru <wataru@codegraph.jp>
2016-01-28 12:12:22 +0900

    remove Wordpress emoji code

--- wp-content/themes/_s/functions.php
+++ wp-content/themes/_s/functions.php
@@ -76,6 +76,10 @@ function _s_setup() {
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
+
+	// Remove Wordpress emoji code.
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'wp_print_styles', 'print_emoji_styles', 10 );
 }
 endif;
 add_action( 'after_setup_theme', '_s_setup' );
