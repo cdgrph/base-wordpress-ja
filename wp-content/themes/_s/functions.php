0426b3eeff57e652365f2355f00332a50fe9930b
wataru <wataru@codegraph.jp>
2016-01-28 12:37:23 +0900

    clean up wp_head

--- wp-content/themes/_s/functions.php
+++ wp-content/themes/_s/functions.php
@@ -76,13 +76,47 @@ function _s_setup() {
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
+}
+endif;
+add_action( 'after_setup_theme', '_s_setup' );
 
+
+/*
+ * Clean up wp_head.
+ */
+function _s_start_cleanup() {
+	// Initialize the cleanup
+	add_action('init', '_s_cleanup_head');
+}
+// WordPress cleanup function
+function _s_cleanup_head() {
+	// EditURI link
+	remove_action( 'wp_head', 'rsd_link' );
+	// Category feed links
+	remove_action( 'wp_head', 'feed_links_extra', 3 );
+	// Post and comment feed links
+	remove_action( 'wp_head', 'feed_links', 2 );
+	// Windows Live Writer
+	remove_action( 'wp_head', 'wlwmanifest_link' );
+	// Index link
+	remove_action( 'wp_head', 'index_rel_link' );
+	// Previous link
+	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+	// Start link
+	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+	// Canonical
+	remove_action('wp_head', 'rel_canonical', 10, 0 );
+	// Shortlink
+	remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+	// Links for adjacent posts
+	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+	// WP version
+	remove_action( 'wp_head', 'wp_generator' );
 	// Remove Wordpress emoji code.
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 	remove_action( 'wp_print_styles', 'print_emoji_styles', 10 );
 }
-endif;
-add_action( 'after_setup_theme', '_s_setup' );
+add_action('after_setup_theme','_s_start_cleanup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
